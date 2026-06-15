22f09e2cde47ff819e35a365bbc50f05c50ef0f6
Antonio Gemesson <antonio.gemesson@ufpi.edu.br>
2026-06-15 14:32:39 -0300

    Adiciona funcao de print

--- src/Assembly/main.s
+++ src/Assembly/main.s
@@ -2,18 +2,15 @@
     HashTable:   .space 80056
     HashTableSize:   .word 10007
     tmp_key: .space 8
-    file: .asciz "/home/agso/Codes/Assembly/riscv/Risc-V_Tokenizer/texts/input.txt"
-    titulo: .asciz "Palavra ----> Frequência"
-    aux: .asciz " ----> "
-    frase: .asciz "Venha Ficar SLA venha, venha.hy ficar, agora, sl, hy hy hy.hy"
-
+    file: .asciz "../../texts/input.txt"
+    
 .text
 .globl main
 
 main:
     #Chama Readfile pra ler o texto
     la a0, file
-    jal readfile # Retorna o texto em a0
+    jal readfile
 
     #Chama StrToLower para fazer o tratamento de maiúsculas
     jal strtolower
@@ -37,49 +34,12 @@ main:
     jal SORTBUCK
 
     #Print dos resultados
-    la t0, HashTableSize
-    lw t0, 0(t0)
-    mv t2, a0
+    la a0, HashTable
+    la a1, HashTableSize
+    lw a1, 0(a1)
+    jal strprint
 
-    la a0, titulo
-    li a7, 4
+fim:
+    li a7, 10
     ecall
 
-    li a0, 10
-    li a7, 11
-    ecall
-    ecall
-
-loop_print:
-    lw t1, 4(t2)
-
-    beq t1, x0, not_print
-
-    lw t1, 0(t2)
-    
-    mv a0, t1
-    li a7, 4
-    ecall
-
-    la a0, aux 
-    ecall
-
-    lw t1, 4(t2)
-
-    mv a0, t1
-    li a7, 1
-    ecall
-
-    li a0, 10
-    li a7, 11
-    ecall
-
-not_print:
-    addi t0, t0, -1
-    addi t2, t2, 8
-    beq t0, x0, e
-    j loop_print
-      
-e: 
-	li a7, 10
-	ecall
---
+++ src/Assembly/strprint.s
@@ -0,0 +1,53 @@
+# ==============================================================================
+# Função: strprint
+# Descrição: Muda todas as letras de uma string terminada em zero (asciz) de maiúsculos para minusculos
+#
+# @param a0: Ponteiro para o endereço base da Hash Table
+# @param a1: Tamanho da Hash Table
+#
+# @return: Void, não tem nenhum retorno
+# ==============================================================================
+.data
+    titulo: .asciz "Palavra\t---->\tFrequência"
+    aux: .asciz "\t---->\t"
+
+.text
+.globl strprint
+strprint:
+    #Print dos resultados
+    mv t0, a0
+    la a0, titulo
+    li a7, 4
+    ecall
+
+    li a0, 10
+    li a7, 11
+    ecall
+
+    li a0, 10
+    li a7, 11
+    ecall
+
+    loop_strprint:
+        beq t0, x0, fim_strprint
+        lw a0, 0(t0)
+        li a7, 4
+        ecall
+
+        la a0, aux
+        li a7, 4
+        ecall
+        
+        lw a0, 4(t0)
+        li a7, 1
+        ecall
+
+        li a0, 10
+        li a7, 11
+        ecall
+
+        addi, t0, t0, 8
+        j loop_strprint
+
+fim_strprint:
+    ret
